9de70a1ce247ef5607557004bae52c49cc713609
Gulyx <guglielmo.deangelis@iasi.cnr.it>
2023-12-13 17:31:58 +0100

    Fixed a typo in the header

--- src/main/java/it/cnr/iasi/saks/xacmut/cli/MutantsCreatorCLI.java
+++ src/main/java/it/cnr/iasi/saks/xacmut/cli/MutantsCreatorCLI.java
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with GROUCHO.  If not, see <https://www.gnu.org/licenses/>
+ * along with XACMUT-CLI.  If not, see <https://www.gnu.org/licenses/>
  *
  */
 package it.cnr.iasi.saks.xacmut.cli;
